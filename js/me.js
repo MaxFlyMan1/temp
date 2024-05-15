bbabafeb09fc87bd0788cbef90260c89da76aba5
MaxFlyMan <69834713+MaxFlyMan1@users.noreply.github.com>
2024-05-15 18:46:14 -0400

    Add files via upload

---
+++ js/me.js
@@ -0,0 +1,37 @@
+function yt() {
+  window.open("https://www.youtube.com/@maxflyman171", '_blank');
+}
+
+function tt() {
+  window.open("https://www.tiktok.com/@maxflymann", '_blank');
+}
+
+function disc() {
+  window.open("https://discord.gg/3SzwSmtdWd", '_blank');
+}
+
+function twch() {
+  window.open("https://www.twitch.tv/maxflyman1", '_blank');
+}
+
+function twit() {
+  window.open("https://twitter.com/MaxFlyMann", '_blank');
+}
+
+function redd() {
+  window.open("https://www.reddit.com/user/MaxFlyman", '_blank');
+}
+
+function spot() {
+  window.open("https://open.spotify.com/user/joi9zmlvrwp8atmquz9f5zmff", '_blank');
+}
+
+function cash() {
+  window.open("https://cash.app/$MaxFlyMan", '_blank');
+}
+
+
+function meow() {
+  meowsound = new Audio("../media/audio/meow.wav");
+  meowsound.play();
+}
