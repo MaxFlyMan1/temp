7e24730d2f2aa73b5cf6654bfa24e08b59d54c7a
MaxFlyMan <69834713+MaxFlyMan1@users.noreply.github.com>
2024-05-16 19:28:14 -0400

    Update me.js

--- js/me.js
+++ js/me.js
@@ -1,36 +1,3 @@
-function whichBrowser() {
-  if (isFirefox()) {
-    return "firefox";
-  } else if (isChrome()) {
-    return "chrome";
-  } else {
-    return "idfk";
-  }
-}
-  
-function agentHas(keyword) {
-  return navigator.userAgent.toLowerCase().search(keyword.toLowerCase()) > -1;
-}
-  
-function isChrome() {
-  return agentHas("CriOS") || agentHas("Chrome") || !!window.chrome;
-}
-  
-function isFirefox() {
-  return agentHas("Firefox") || agentHas("FxiOS") || agentHas("Focus");
-}
-
-function fixbuttons() {
-  if (whichBrowser() == "firefox"){
-    button = document.getElementsByClassName("button")
-    for (let i = 0; i < button.length; i++) {
-      button[i].style.width = "24.4%"
-    }
-  }
-}
-
-window.onload = fixbuttons()
-
 function yt() {
   window.open("https://www.youtube.com/@maxflyman171", '_blank');
 }
